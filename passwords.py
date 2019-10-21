5e79d7886f317c76e12d300d6ea865d735594d33
George Flanagin <me@georgeflanagin.com>
2019-10-21 10:59:34 -0400

    Added decorators, fixed help, added --bare option.

--- passwords.py
+++ passwords.py
@@ -57,7 +57,6 @@ def load_dict(s:str) -> set:
     with open(s) as f:
         w = set(f.read().split())
     timer.stop()
-    print("dictionary loaded in {} seconds.".format(round(timer.stop() - start_time, 3)))
     return w
 
 words = load_dict(dictionary_location) if dictionary_location else set()
@@ -94,7 +93,7 @@ def decomp(s:str, debug:bool=False) -> tuple:
         row_sums[i] = numpy.sum(t[i,:])
         column_sums[i] = numpy.sum(t[:,i])
 
-    if debug: print("Column sums: {}".format(tuple(column_sums)))
+    if debug: print("Topological analysis vector: {}".format(tuple(column_sums)))
     fragments = []
     i = 0
     while i < dimension:
@@ -109,7 +108,7 @@ def decomp(s:str, debug:bool=False) -> tuple:
     return fragments, column_sums
 
 
-def make_safe(s:str,a:str) -> str:
+def make_safe(s:str, a:str) -> str:
     """
     UR's experience is that lower case ells and cap O create problems.
     Swap l for L and O for o, and then swap anything not in the
@@ -125,6 +124,15 @@ def make_safe(s:str,a:str) -> str:
     return s
 
 
+def password_eval(s:str) -> None:
+    """
+    Evaluate the string as a potential password, and then exit.
+    """
+    fragments, column_sums = decomp(s, True)
+    sys.exit(os.EX_OK)
+
+
+@trap
 def password_gen(my_args:argparse.Namespace) -> list:
     """
     Generate passwords and estimate their unlikeliness (if that is even
@@ -223,44 +231,59 @@ def password_gen(my_args:argparse.Namespace) -> list:
                 entropic_bits, 
                 timer.lap()-timer.start(), 
                 entropic_ratio,
-                len(password)/sum(column_sums)))
+                sum(column_sums)/len(password))
+                )
 
 
     done_time = time.time()
-    print('\n{} branches, with {} branches pruned.\n'.format(ops, rejects))
-
-    print('{} seconds creating passwords.'.format(round(done_time - sources_built,2)))
+    if not my_args.bare:
+        print('\n{} branches, with {} branches pruned.\n'.format(ops, rejects))
+        print('{} seconds creating passwords.'.format(round(done_time - sources_built,2)))
 
     return passwords
             
+
+def fmt_opts(my_args:argparse.Namespace) -> str:
+    """
+    Replace the _ with -, and return a list of the options as the user
+        probably wrote them on the command line.
+    """
+    opt_string = ''
+    for _ in sorted(vars(my_args).items()):
+        opt_string += " --"+ _[0].replace("_","-") + " " + str(_[1]) + "\n"
+    return (opt_string + "\n")
+
 
 def do_help() -> None:
     """
     Usage: passwords [opts]
 
-    "We have the best passwords."
+    "I'm very highly educated. I know passwords, I know the best passwords. But 
+    there's no better password than stupid."
 
-            -- Donald J. Trump
+                            -- Donald J. Trump, South Carolina, Dec 30, 2015.
 
     Options are:
 
-        -? / --help : you are reading it.
+        --assist : you are reading it.
+
         -a / --alphabet : allowed chars in password, defaults to quite
+            the usual list of alpha, digits, and the most common "punctuation"
+            characters.
+        --bare : Just the passwords as output. Skip the narrative. Primarily
+            useful if the output is being fed to another program.
         -b / --bits : bits of "entropy" required in each password. YES YES
             I know this is not even sort of what is meant by 'entropy' in
             physics, but I also know the word has taken root and it is
             a measure of like, like something. Whatever. Actually. 
-        -d / --debug : provide some mostly not-useful stats while running.
-        -g / --guesses : a scaling factor for billion (10^9) guesses per second
-            for scaling the estimate of brute force cracking. This value
-            is used to figure the number of days required for the 
-            password to show up in a list of passwords used in a cracking
-            attempt.
+        -d / --debug : provide some mostly not-too-useful stats while running.
+        -e / --eval : evaluate this string as a password, rather than generating
+            one. This option must be used alone.
         -l / --lists : location of the 'well known words' lists. This should
             be a wildcard directory/filename. The default value is 
             $PWD/wordlist*txt. The lists are assumed to be a filter
             (math meaning) on the smallest list. 
-        -n / --number : how many passwords do you want? defaults to 10.
+        -n / --number : how many passwords do you want? defaults to 10. 
         -w / --words : location of the dictionary file.
         -x / --max-length : how long can they be? defaults to 40.
         -z / --min-length : how long must each password be? defaults to 16.
@@ -268,68 +291,99 @@ def do_help() -> None:
 
     Prints the options and a list of passwords. Example shown below.
 
+    Output is to stdout, so you may use standard I/O redirection.
 
-288 branches, with 0 branches pruned.
+---- Sample Output Begins ----
 
-1.63 seconds creating passwords.
+240 branches, with 0 branches pruned.
+
+1.29 seconds creating passwords.
 
 Passwords generated by using the following options:
 
-  --alphabet abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789/+=!~-? --bits 50 --debug False --guesses 1 --h False --lists /home/george/python.programming/passwordgenerator/wordlist*txt --max-length 40 --min-length 16 --number 12 --words /usr/share/dict/words
+ --alphabet abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789/+=!~-?
+ --bare False
+ --bits 50
+ --debug False
+ --eval None
+ --h False
+ --lists /home/george/python.programming/passwordgenerator/wordlist*txt
+ --max-length 40
+ --min-length 16
+ --number 10
+ --words /usr/share/dict/words
 
-     :: Password                                  :: Len ::   Bits ::      Days ::    CPU sec
+
+     :: Password                                  :: Len ::   Bits ::    CPU ::    Q1 ::    Q2
 ====================================================================================================
-   0 :: osteadfastnessesspring-Lockchibchan       ::  35 ::   61.3 :: 3.324e+04 ::      0.209
-   1 :: WScEYFVunLikeLiness                       ::  19 ::   51.8 :: 4.460e+01 ::       0.07
-   2 :: churchinesskuabashedLyJ                   ::  23 ::   51.8 :: 4.684e+01 ::      0.139
-   3 :: cWSECNAVAustrogaean4                      ::  20 ::   52.5 :: 7.205e+01 ::      0.216
-   4 :: overcreduLousness0Fitzpat3c               ::  27 ::   50.5 :: 1.801e+01 ::      0.139
-   5 :: LemassErnJ6565EfruitLet                   ::  23 ::   74.5 :: 3.166e+08 ::      0.139
-   6 :: BLoxbergbpKuehn6                          ::  16 ::   50.5 :: 1.801e+01 ::      0.139
-   7 :: 5u?jL0?tarsadenitis                       ::  19 ::   49.0 :: 6.598e+00 ::       0.07
-   8 :: 2-8peripLasticgADC                        ::  18 ::   49.0 :: 6.598e+00 ::      0.139
-   9 :: HktopchromeHgneoLogies                    ::  22 ::   56.5 :: 1.218e+03 ::      0.158
-  10 :: -AIautonomouss662                         ::  17 ::   47.6 :: 2.538e+00 ::       0.07
-  11 :: 7pSIcosmopoLitismzLecythidaceae           ::  31 ::   62.5 :: 7.378e+04 ::      0.139
+   0 :: BsizygiaGharsh-soundingictic              ::  28 ::   68.0 :: 0.231  :: 1.04  :: 4.0
+   1 :: 29FganeKiewitcheerLess                    ::  22 ::   68.3 :: 0.209  :: 0.788 :: 5.045
+   2 :: 4VQ6aLLegoricaLPthiram                    ::  22 ::   60.8 :: 0.14   :: 0.574 :: 4.045
+   3 :: cassinoidNGjLaquais                       ::  19 ::   54.1 :: 0.14   :: 0.349 :: 4.211
+   4 :: o78L7kTracheLospermum0                    ::  22 ::   50.9 :: 0.071  :: 0.843 :: 3.909
+   5 :: runcLoaking7nnonfreeman                   ::  23 ::   51.8 :: 0.141  :: 0.734 :: 5.565
+   6 :: s9abashedGSj05+8                          ::  16 ::   60.0 :: 0.071  :: 0.449 :: 4.0
+   7 :: rupestraLyhypothyroidismsphenopetrosaL    ::  38 ::   62.3 :: 0.211  :: 0.681 :: 6.421
+   8 :: -bputted3mfqMJB~                          ::  16 ::   69.3 :: 0.07   :: 1.018 :: 1.625
+   9 :: 2ETuscanCMontcLair                        ::  18 ::   51.1 :: 0.14   :: 1.087 :: 4.056
+
+
+---- Sample Output Ends ----
+
+    Bits -- A calculation of the vague notion of password entropy. The password entropy is 
+            cumulatively calculated as it is constructed, and then the password is evaluated
+            by decomposition. The lesser of the two calculations is shown.
+
+    CPU  -- How many CPU seconds were spent on each password.
+
+    Q1   -- A number greater than zero that reflects the ratio between reverse and forward
+            calculations of entropy. Higher is better.
+
+    Q2   -- A number greater than zero that is a rough measure of the likelihood that this
+            password is vulnerable to topological analyses. If you analyze a password, you
+            will see the order N matrix, where N is the length of the password. A higher
+            number is a worse score. 
+
+---- Analysis of password #5 in the above list. ----
+
+python passwords.py -e runcLoaking7nnonfreeman
+
+[[1 1 1 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0]
+ [0 2 2 1 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0]
+ [0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0]
+ [0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0]
+ [0 0 0 0 2 2 1 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0]
+ [0 0 0 0 0 1 1 1 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0]
+ [0 0 0 0 0 0 3 3 2 2 1 0 0 0 0 0 0 0 0 0 0 0 0]
+ [0 0 0 0 0 0 0 3 3 2 1 0 0 0 0 0 0 0 0 0 0 0 0]
+ [0 0 0 0 0 0 0 0 2 2 1 0 0 0 0 0 0 0 0 0 0 0 0]
+ [0 0 0 0 0 0 0 0 0 1 1 0 0 0 0 0 0 0 0 0 0 0 0]
+ [0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0]
+ [0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0]
+ [0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0]
+ [0 0 0 0 0 0 0 0 0 0 0 0 0 3 3 2 1 1 1 1 1 1 1]
+ [0 0 0 0 0 0 0 0 0 0 0 0 0 0 1 1 0 0 0 0 0 0 0]
+ [0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0]
+ [0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 3 3 2 2 1 1 1]
+ [0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 3 3 2 1 0 0]
+ [0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 1 1 0 0 0]
+ [0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 1 1 0 0]
+ [0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 2 2 1]
+ [0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 1 1]
+ [0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0]]
+
+Topological analysis vector: (1, 3, 3, 1, 2, 3, 5, 7, 7, 7, 4, 0, 0, 3, 4, 3, 4, 7, 7, 7, 6, 5, 4)
 
     """
     print(do_help.__doc__)
     sys.exit(os.EX_OK)
 
 
-
-def passwords_main():
-    """
-    Use the command line arguments, and generate some passwords. Good passwords.
-    """
-    global words
-    default_location = os.environ.get('PWD') + os.sep + 'wordlist*txt'
-
-    parser = argparse.ArgumentParser()
-    parser.add_argument('-?', '--h', action='store_true')
-    parser.add_argument('-a', '--alphabet', type=str, 
-        default=default_alphabet)
-    parser.add_argument('-b', '--bits', type=int, default=50)
-    parser.add_argument('-d', '--debug', action='store_true', default=False)
-    parser.add_argument('-g', '--guesses', type=int, default=1)
-    parser.add_argument('-l', '--lists', type=str, default=default_location)
-    parser.add_argument('-n', '--number', type=int, default=10)
-    parser.add_argument('-w', '--words', type=str, 
-        default=dictionary_location)
-    parser.add_argument('-x', '--max-length', type=int, default=40)
-    parser.add_argument('-z', '--min-length', type=int, default=16)
-    my_args = parser.parse_args()
-    if my_args.h: do_help()
-
-    if my_args.words != dictionary_location: words = load_dict(my_args.words)
-
-    passwords = password_gen(my_args)
-
-    print("\nPasswords generated by using the following options:\n")
-    opt_string = ' '
-    for _ in sorted(vars(my_args).items()):
-        opt_string += " --"+ _[0].replace("_","-") + " " + str(_[1]) + "\n"
-    print(opt_string + "\n")
+@trap
+def passwords_output(my_args:argparse.Namespace, passwords:list) -> None:
+    if my_args.bare:
+        print("\n".join([_[0] for _ in passwords]))
+        return
 
     print(" :: ".join([
         '    ',
@@ -337,7 +391,7 @@ def passwords_main():
         'Len',
         'Bits'.rjust(6),
         # 'Days'.rjust(9),
-        'CPU sec'.rjust(10),
+        'CPU'.rjust(6),
         'Q1'.rjust(5),
         'Q2'.rjust(5)
         ]))
@@ -351,12 +405,61 @@ def passwords_main():
             p[0].ljust(my_args.max_length+1), 
             str(len(p[0])).rjust(3),
             str(round(p[1],1)).rjust(6), 
-            # "{:.3e}".format(math.pow(2,p[1])/(my_args.guesses*(10**9)*86400)),
-            str(round(p[2]-deduction,3)).ljust(10),
+            str(round(p[2]-deduction,3)).ljust(6),
             str(round(p[-2],3)).ljust(5),
             str(round(p[-1],3)).ljust(6)
             ]))
         deduction = p[2]
+    
+
+
+def passwords_main():
+    """
+    Use the command line arguments, and generate some passwords. Good passwords.
+    """
+    global words
+    default_location = os.environ.get('PWD') + os.sep + 'wordlist*txt'
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-?', '--h', action='store_true')
+    parser.add_argument('--assist', action='store_true',
+        help='Show the long version of the help text.')
+    parser.add_argument('-a', '--alphabet', type=str, default=default_alphabet,
+        help='A string that defines the allowable characters in all passwords.')
+    parser.add_argument('-b', '--bits', type=int, default=50,
+        help='Lower bound of the so-called "entropy" of the passwords.')
+    parser.add_argument('--bare', action='store_true', default=False,
+        help='This option will print the passwords only, no stats, no analysis.')
+    parser.add_argument('-d', '--debug', action='store_true', default=False,
+        help='Causes the program to narrate its progress.')
+    parser.add_argument('-e', '--eval', type=str, default=None,
+        help='If this option is used, the string given will be analyzed as a password.')
+    parser.add_argument('-l', '--lists', type=str, default=default_location, 
+        help='Filename spec containing lists of common words.')
+    parser.add_argument('-n', '--number', type=int, default=10,
+        help='Number of desired passwords.')
+    parser.add_argument('-w', '--words', type=str, default=dictionary_location,
+        help='Location of the system dictionary, or equivalent.')
+    parser.add_argument('-x', '--max-length', type=int, default=40,
+        help='Max len of a generated password.')
+    parser.add_argument('-z', '--min-length', type=int, default=16,
+        help='Passwords will be at least this long.')
+
+    my_args = parser.parse_args()
+    if my_args.assist: do_help()
+    if my_args.eval is not None:
+        password_eval(my_args.eval)
+
+    if my_args.words != dictionary_location: words = load_dict(my_args.words)
+
+    passwords = password_gen(my_args)
+
+    if not my_args.bare:
+        print("\nPasswords generated by using the following options:\n")
+        print(fmt_opts(my_args))
+    
+    passwords_output(my_args, passwords)
+
 
 if __name__ == "__main__":
     passwords_main()
